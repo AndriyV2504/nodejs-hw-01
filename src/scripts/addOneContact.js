7357cc6c26c7b684784e1bec54538fb44053aebd
AndriyV2504 <andreyvolyanik@gmail.com>
2024-09-07 11:32:32 +0300

    addOneContacts.js

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,17 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const contacts = await readContacts();
+  const newContact = {
+    id: 6,
+    name: 'Zeb',
+    phone: '(142) 5701520',
+    email: 'zhorburgh5@springer.com',
+    job: 'Health Coach I',
+  };
+  contacts.push(newContact);
+  writeContacts(contacts);
+};
 
 addOneContact();
