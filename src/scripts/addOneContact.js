0381b86938f01325a22f819d3236b0760d05fe18
AndriyV2504 <andreyvolyanik@gmail.com>
2024-09-07 11:49:11 +0300

    correct addOneContact.js

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,17 +1,13 @@
+import { createFakeContact } from '../utils/createFakeContact.js';
 import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
 export const addOneContact = async () => {
   const contacts = await readContacts();
-  const newContact = {
-    id: 6,
-    name: 'Zeb',
-    phone: '(142) 5701520',
-    email: 'zhorburgh5@springer.com',
-    job: 'Health Coach I',
-  };
+  const newContact = createFakeContact();
   contacts.push(newContact);
-  writeContacts(contacts);
+  await writeContacts(contacts);
+  console.log('Added new contact:', newContact);
 };
 
 addOneContact();
